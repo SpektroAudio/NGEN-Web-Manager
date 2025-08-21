e0276a82742cd51bc5d122b33aa7576364dfc2d3
Icaro Ferre <icaroferre@gmail.com>
2025-08-21 15:47:56 -0300

    Fixed repository URL

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,44 +1,83 @@
 <template>
+  <div class="flex justify-center items-center space-x-8">
+    <Button
+      as="a"
+      href="https://spektroaudio.com/ngen"
+      icon="pi pi-link"
+      target="_blank"
+      class="text-xs px-6"
+      label="NGEN WEBPAGE"
+      size="small"
+      severity="secondary"
+      rounded
+    />
+    <Button
+      as="a"
+      href="https://ngen.spektroaudio.com"
+      icon="pi pi-book"
+      target="_blank"
+      class="text-xs px-6"
+      label="NGEN USER MANUAL"
+      size="small"
+      severity="secondary"
+      rounded
+    />
+    <Button
+      as="a"
+      href="https://spektroaudio.com"
+      icon="pi pi-globe"
+      target="_blank"
+      class="text-xs px-6"
+      label="SPEKTROAUDIO.COM"
+      size="small"
+      severity="secondary"
+      rounded
+    />
+    <Button
+      as="a"
+      href="https://spektroaudio.com"
+      icon="pi pi-github"
+      target="_blank"
+      class="text-xs px-6 rounded-xl"
+      label="REPOSITORY"
+      size="small"
+      severity="secondary"
+    />
+  </div>
 
-    <div class="flex justify-center items-center space-x-8">
-      <Button as="a"  href="https://spektroaudio.com/ngen" icon="pi pi-link" target="_blank" class="text-xs px-6" label="NGEN WEBPAGE" size="small" severity="secondary" rounded  />
-      <Button as="a"  href="https://ngen.spektroaudio.com" icon="pi pi-book" target="_blank" class="text-xs px-6" label="NGEN USER MANUAL" size="small" severity="secondary" rounded  />
-      <Button as="a"  href="https://spektroaudio.com" icon="pi pi-globe" target="_blank" class="text-xs px-6" label="SPEKTROAUDIO.COM" size="small" severity="secondary" rounded  />
-      <Button as="a"  href="https://spektroaudio.com" icon="pi pi-github" target="_blank" class="text-xs px-6 rounded-xl" label="REPOSITORY"  size="small" severity="secondary"   />
-
-    </div>
-
-    <div class="text-[9px] font-mono opacity-20">
-        VERSION {{version}} - {{lastCommitDate}}
-    </div>
-
+  <div class="text-[9px] font-mono opacity-20">
+    VERSION {{ version }} - {{ lastCommitDate }}
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        version: {
-            type: Number,
-            required: true
-        }
+  props: {
+    version: {
+      type: Number,
+      required: true,
     },
-    data() {
-        return {
-            lastCommitDate: '', // Initialize the lastCommitDate property
-        };
+  },
+  data() {
+    return {
+      lastCommitDate: "", // Initialize the lastCommitDate property
+    };
+  },
+  mounted() {
+    this.fetchLastCommitDate();
+  },
+  methods: {
+    async fetchLastCommitDate() {
+      try {
+        const response = await fetch(
+          "https://api.github.com/repos/SpektroAudio/NGEN-Web-Manager/releases?per_page=1",
+        );
+        const data = await response.json();
+        this.lastCommitDate = new Date(data[0].published_at).toLocaleString(); // Format the date
+      } catch (error) {
+        console.error("Error fetching last commit date:", error);
+      }
     },
-    mounted() {
-        this.fetchLastCommitDate();
-    },
-    methods: {
-        async fetchLastCommitDate() {
-            try {
-                const response = await fetch('https://api.github.com/repos/SpektroAudio/DrumGenTemplateEditor/releases?per_page=1');
-                const data = await response.json();
-                this.lastCommitDate = new Date(data[0].published_at).toLocaleString(); // Format the date
-            } catch (error) {
-                console.error('Error fetching last commit date:', error);
-            }
-        },
-    }
-}
+  },
+};
 </script>
+
